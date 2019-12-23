c761e774af21a2ad24fac516dd02d734285a4c3d
Tong <superzjt526@users.noreply.github.com>
2019-12-23 18:38:27 +0800

    优化
    新增忽略规则、根据阿里编码规约优化代码

--- src/test/java/com/yunhesoft/tm4/DbDictionaryApplicationTest.java
+++ src/test/java/com/yunhesoft/tm4/DbDictionaryApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DbDictionaryApplicationTests {
+class DbDictionaryApplicationTest {
 
 	@Test
 	void contextLoads() {
